bfcba8285f4e889317cf77108072a3e4e8eb6dc1
Ramplex <ramplexstorage@gmail.com>
2024-11-11 16:28:21 -0500

    Create Animedetails.vue

---
+++ src/routes/Animedetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Anime Details for ID: {{ $route.params.id }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimeDetails',
+  props: ['id'], // To receive the anime ID from the route
+
+  mounted() {
+    // You'll likely want to fetch the anime details here using this.id
+    // ...
+  }
+};
+</script>
